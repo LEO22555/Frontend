6c2f2aedeb51981df6219915620074e20d6c25c9
LEO22555 <lemartinezba@unal.edu.co>
2023-03-08 17:07:05 -0500

    version 2

--- calificacciones.js
+++ calificacciones.js
@@ -1,5 +1,14 @@
 function calcular_definitva(p1,p2,p3,e1,t1){
     let promedio=0.55*(p1+p2+p3)+0.3*e1+0.15*t1;
     return promedio;
-
 }
+
+var a;
+a=50
+
+if(a<100){
+    let a=40;
+    console.log(a)
+}
+
+console.log(a);
