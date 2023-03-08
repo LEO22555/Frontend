30f1d713d5d7cdc1bdf20ec4d41e23313ed066d5
LEO22555 <lemartinezba@unal.edu.co>
2023-03-08 17:07:42 -0500

    version 2

---
+++ script.js
@@ -0,0 +1,37 @@
+// let: para variables de ambito local
+// var: para variables de ambito global
+// const: valor una vez asignado nunca cambia
+
+// function calcular(base,altura){
+//     const area = base*altura;
+//     const perimetro = 2*(base+altura);
+//     return [area,perimetro];
+// }
+
+
+// function calcular_hipotenusa(a,b){
+//     const hipotenusa= Math.sqrt(a**2+b**2);
+//     return hipotenusa;
+// }
+
+// let resultado = calcular_hipotenusa(10,20);
+// console.log(resultado);
+
+// function calculadora(a,b){
+//     document.writeln(a+b);
+//     document.writeln(a-b);
+//     document.writeln(a*b);
+//     document.writeln(a/b);
+
+//     console.log(a,b);
+//     console.warn(a,b);
+//     console.error(a,b);
+//     console.info(a,b);
+    
+// }
+
+// calculadora(5,2);
+
+function media(){
+    let campo_x= document.getElementById('x')
+}
